15b1b75de0317723653a353cf264251f7d2fbe51
Yomi Ikuru <abayomi.ikuru@gmail.com>
2022-02-01 21:41:55 +0000

    Initial commit

---
+++ micropython/main.py
@@ -0,0 +1,8 @@
+# from machine import Pin, I2C
+# from ssd1306 import SSD1306_I2C
+
+def main():
+    print("Welcome to RT-Thread MicroPython!")
+    
+if __name__ == '__main__':
+    main()
---
+++ software/src/display.cpp
@@ -0,0 +1,5 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
